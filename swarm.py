22403e0bc4e0a552293727e92236e5f1559e463b
lvanderlyn <lindsey.vanderlyn@students.olin.edu>
2014-10-12 15:38:48 -0400

    Visualizing by saving images to look at swarm trend, peaks is giving errors

--- swarm.py
+++ swarm.py
@@ -2,6 +2,8 @@ import numpy as np
 import random 
 import matplotlib.pyplot as plt 
 import matplotlib.cm as cm
+import pylab 
+from mpl_toolkits.mplot3d import Axes3D
 mapping = cm.jet
 class Swarm(object):
     """Wrapper class performs optimization between particle objects"""
@@ -19,12 +21,17 @@ class Swarm(object):
         RETURNS(position, functionValue)
         """
         iteration = 0
-        converged = False 
+        converged = False
+        plt.hold(True)
+        fileadd = 0;
         while not converged:
         # for i in range(4):
             self.update()
             converged = self.checkConvergence(iteration)
             iteration = iteration+1
+            if self.disp:
+                pylab.savefig('peaks%d.jpg' %fileadd)
+                fileadd=fileadd+1
         return [self.overBestPos, self.overBestVal]
 
     def update(self):
@@ -46,13 +53,17 @@ class Swarm(object):
         print(self.overBestVal)
         # print(self.overBestPos, self.overBestVal)
         # print("Current Position, Current Velocity, Current value")
-        for index in range(len(self.swarm)):     
+        for index in range(len(self.swarm)): 
+            z= function.evaluate(X,Y)
+            plt.hold(True)
+            cs = plt.contour(X, Y, z)
+            if self.disp:
+                c = mapping(int(255/(index+1)))
+                plt.plot(self.swarm[index].position[0], self.swarm[index].position[1], '*', mfc = c, mec = c)    
             self.swarm[index].updateVelocity(self)
             self.swarm[index].updatePosition()
             self.swarm[index].evaluateFunction()
-            if self.disp:
-                c = mapping(int(255/(index+1)))
-                plt.plot(self.swarm[index].position[0], self.swarm[index].position[1], '*', mfc = c, mec = c)
+
             # print(Particle.position, Particle.velocity[0], Particle.functionValue)
 
     def checkConvergence(self, iteration):
@@ -61,12 +72,12 @@ class Swarm(object):
         """
         threshold = abs(0.01*self.overBestVal)
         stdev = np.std(np.array([particle.bestXYZ[2] for particle in self.swarm]))
-        return stdev <= threshold or iteration > 1000
+        return stdev <= threshold or iteration > 200
 
 class Particle(object):
     """Class for creating each of the particles, handles knowledge/optimization for single particle"""
     def __init__(self, function):
-        self.position = np.array([random.uniform(-20,20), random.uniform(-20,20)]) # randrange gives 
+        self.position = np.array([random.uniform(-120,120), random.uniform(-120,120)]) # randrange gives 
         self.velocity = np.array([random.uniform(-1,1), random.uniform(-1,1)]) # CHANGE: Set a random x and y velocity, with +/-  -> sends out particles in random direction
         self.function = function
         self.functionValue = 0
@@ -96,9 +107,9 @@ class Particle(object):
             RETUNS(void)
         """
         # Velocity equation parameters that control efficacy and behavior of the PSO, selected arbitrarily by us 
-        omega= 1.0     # coefficient for influence of currect velocity
-        psi_loc= 0.5   # coefficient for influence of local best
-        psi_glob= 0.25  # coefficient for influence of global best 
+        omega= .30     # coefficient for influence of currect velocity
+        psi_loc= 0.40   # coefficient for influence of local best
+        psi_glob= 0.30  # coefficient for influence of global best 
         #calculates random weights between 0 and 1 (non-inclusive) for influence of individual (local) or social (global) best
         # use maximum() to ensure that random weight is greater than 0 (fullfils the non-inclusive requirement)
         randLocalWeight= max(0.01, random.random())
@@ -134,17 +145,19 @@ if __name__ == '__main__':
             RETURNS: float of the result"""
 
             #Current function comes from MATLAB Peaks function
-            # return 3*(1-x)**2*np.exp(-(x**2)) - (y+1)**2 - 10 * (x/5 - x**3 - y**5) * np.exp(-(x**2) - (y**2)) - (1/3)*np.exp(-((x+1)**2) - y**2)
-            return -np.power((x-3),2) - np.power(y, 2)-3
+            return 3*(np.power(1-x),2).dot(np.exp(-np.power(x,2))) - np.power((y+1),2) - 10 * (x/5 - np.power(x,3) - np.power(y,5)).dot(np.exp(-np.power(x,2) - np.power(y,2))) - (1/3)*np.exp(-np.power((x+1),2) - np.power(y,2))
+            # return -np.power((x-50),2) - np.power(y, 2)-3
     N = 100
-    x = np.linspace(-50.0, 50.0, N)
-    y = np.linspace(-50.0, 50.0, N)
-
-    X, Y = np.meshgrid(x, y)
-
+    x = np.linspace(-150.0, 150.0, N)
+    y = np.linspace(-150.0, 150.0, N)
     function= my_function()
-    z= function.evaluate(X,Y)
-    cs = plt.contour(X, Y, z)
-    PSO = Swarm(3, function, True)
-    PSO.solve()
+    X, Y = np.meshgrid(x, y)
+    Z = function.evaluate(X,Y)
+    # PSO = Swarm(15, function, True)
+    # PSO.solve()
+    fig = plt.figure()
+    ax = fig.add_subplot(111,projection = '3d')
+    ax.plot_surface(X,Y,Z)
     plt.show()
+
+
